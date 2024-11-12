1dbbad51b3c64c57927658a1f8c3aaaf1ff1210d
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2024-11-12 14:23:17 +0900

    git init again

---
+++ src/main/java/umc/spring/domain/Category.java
@@ -0,0 +1,31 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<User> userList = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Store> storeList = new ArrayList<>();
+}
